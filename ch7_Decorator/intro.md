47afed92a712e21a46474e59f0cd19f544b0e113
edward <huogiafei@163.com>
2020-09-03 18:15:15 +0800

    update decorator pattern

---
+++ ch7_Decorator/intro.md
@@ -0,0 +1,50 @@
+Decorator 装饰者模式
+====
+
+### 定义 
+
+动态地给对象添加额外的职责或者行为，相比于继承更加灵活
+   
+![cover](./ch7_cover.jpg)
+
+> 对白:  You wear a mask for so long, you forget who you were beneath it. 《V字仇杀队》
+
+### 解决的问题
+
+为了适应业务扩展，经常要增加功能或者添加子类，使用装饰者模式可以灵活的增加功能
+
+### 场景
+
+* 系统的埋点和数据上报
+* 表单增加字段，需要新增验证的条件
+
+### 优点
+
+* 需要扩展函数时，不会增加耦合度
+
+### 缺点
+
+* 多层的嵌套会影响性能和增加复杂度
+
+### 提示
+
+
+### 类图
+
+![uml](./uml.png)
+
+### 参考链接
+
+> https://www.runoob.com/design-pattern/decorator-pattern.html      
+>
+
+
+
+
+
+
+    
+
+
+ 
+
---
+++ ch7_Decorator/ls1_favorStatistics.js
@@ -0,0 +1,41 @@
+//收藏功能
+function favor() {
+    //模拟请求
+    console.log('收藏成功');
+    return 200
+}
+
+Function.prototype.before = function (beforeFn) {
+    let _self = this;
+    return function () {
+        beforeFn.apply(this, arguments);
+        return _self.apply(_self, arguments);
+    }
+};
+
+Function.prototype.after = function (afterFn) {
+    let _self = this;
+    return function () {
+        let result = _self.apply(_self, arguments);
+        afterFn.apply(this, arguments);
+        return result;
+    }
+};
+
+//装饰模式
+favor = favor.before(function () {
+    console.log('开始计时');
+    console.time('favorTime');
+});
+
+favor = favor.after(function () {
+    console.log('结束计时');
+    console.timeEnd('favorTime');
+}).after(function(){
+    console.log('GA上报数据')
+});
+
+console.log(favor());
+
+
+
