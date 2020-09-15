968326c2c1b9e8210ce710d3a6aad45d776be1d7
edward <huogiafei@163.com>
2020-09-15 16:44:08 +0800

    update State pattern

--- ch8_State/ls1_player/player.js
+++ ch8_State/ls1_player/player.js
@@ -1,21 +1,14 @@
+import frequency from './assets/frequency.js';
+import marioBook from './assets/marioBgm.js';
 
 window.onload = function () {
-
     const $btn = document.querySelector('#btn');
 
     $btn.addEventListener('click', () => {
         const audioCtx = new AudioContext();
         const player = new BitPlayer(audioCtx);
         player.init();
-        player.setFrequency(300)
         player.play();
-
-       /* player.setFrequency(400)
-        player.play()
-        player.stop();*/
-        /*setTimeout(() => {
-            player.stop();
-        }, 4000)*/
     });
 };
 
@@ -24,6 +17,7 @@ class BitPlayer {
     constructor(context) {
         this.context = context;
         this.initFinish = false;
+
     }
 
     init() {
@@ -34,29 +28,43 @@ class BitPlayer {
             this.oscillator.connect(this.gainNode);
             this.gainNode.connect(this.context.destination);
             this.initFinish = true;
-
+            console.log(this.oscillator)
         }
-    }
-
-    setFrequency(value){
-        this.oscillator.frequency.value = value;
     }
 
     play() {
         this.init();
         this.gainNode.gain.setValueAtTime(.5, this.context.currentTime);
-        this.oscillator.start();
-        this.stop();
+        this.playMelody(marioBook);
     }
 
-    stop() {
-        //声音淡出
-        this.gainNode.gain.exponentialRampToValueAtTime(.001, this.context.currentTime + 1);
-        this.oscillator.stop(this.context.currentTime + 1);
-    }
+    playMelody(book) {
+        const {speed, score} = book;
+        const ctx = this.context;
+        const gn = this.gainNode;
+        let prevTime = 0;
+        score.forEach(item => {
+            let start = ctx.currentTime + prevTime;
+            const time = 60 / speed * item[1];
+            const clickTime = 60/speed * .5;
 
-    changeTone(){
-        this.oscillator.stop(this.context.currentTime);
+            const tones = item[0].split('/');
+            tones.forEach(tone=>{
+                let osc = ctx.createOscillator();
+                osc.connect(gn);
+                osc.connect(ctx.destination);
+
+                osc.frequency.value = frequency[tone];
+                gn.gain.setValueAtTime(0, start);
+                gn.gain.linearRampToValueAtTime(.5, start + clickTime * .05);
+                gn.gain.setValueAtTime(.5, start + clickTime * 0.1);
+                gn.gain.linearRampToValueAtTime(0, start + clickTime * 0.5);
+
+                osc.start(ctx.currentTime + prevTime);
+                osc.stop(ctx.currentTime + clickTime + prevTime);
+            })
+            prevTime += time;
+        })
     }
 }
 
--- ch8_State/question.md
+++ ch8_State/question.md
@@ -11,4 +11,5 @@ QUESTION
 https://developer.mozilla.org/zh-CN/docs/Web/API/AudioContext   
 https://zh.wikipedia.org/zh/%E9%9F%B3%E9%AB%98  
 https://zh.wikipedia.org/wiki/%E9%8B%BC%E7%90%B4%E9%8D%B5%E9%A0%BB%E7%8E%87 
-https://www.html5rocks.com/en/tutorials/webaudio/intro/
+https://www.html5rocks.com/en/tutorials/webaudio/intro/     
+https://7tonshark.com/2018-09-16-web-audio-part-1/      
